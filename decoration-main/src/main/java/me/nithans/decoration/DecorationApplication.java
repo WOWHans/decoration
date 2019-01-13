8e13f80f88b46d60850d6875d04072b271a3de7f
Hans <hw.pers@hotmail.com>
2019-01-13 16:59:17 +0800

    build($project-structure): logback & 事务配置

--- decoration-main/src/main/java/me/nithans/decoration/DecorationApplication.java
+++ decoration-main/src/main/java/me/nithans/decoration/DecorationApplication.java
@@ -2,8 +2,12 @@ package me.nithans.decoration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@ImportResource("classpath*:spring/appContext.xml")
 public class DecorationApplication {
 
     public static void main(String[] args) {
