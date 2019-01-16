d355a5b9a7e9386ba1f9e79d77028672a98cd1f2
Hans <hw.pers@hotmail.com>
2019-01-16 20:08:37 +0800

    feat(api): add api module

---
+++ decoration-api/src/main/java/me/nithans/decoration/api/controller/BaseController.java
@@ -0,0 +1,11 @@
+package me.nithans.decoration.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseController {
+
+    private static final Logger log = LoggerFactory.getLogger(BaseController.class);
+
+
+}
